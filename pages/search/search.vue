b099ceffec677237d9fe6892d9736e0d3230c2a5
867097256 <42594754+867097256@users.noreply.github.com>
2019-07-15 17:05:51 +0800

    Add files via upload

---
+++ pages/search/search.vue
@@ -0,0 +1,17 @@
+<template>
+	<view>
+		<zy-search :is-focus="false" :is-block="true" :show-want="false"></zy-search>
+	</view>
+</template>
+
+<script>
+	import zySearch from '../../components/zy-search/zy-search.vue'
+	export default {
+		components: {
+			zySearch
+		}
+	}
+</script>
+
+<style>
+</style>
